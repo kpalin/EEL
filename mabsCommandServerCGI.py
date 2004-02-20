332bc8088fbba8108d628fe719df1c61687c21fb
kpalin <>
2004-02-20 10:57:26 +0000

    For use in apache web server so that the distribution can go through firewalls.

--- mabsCommandServerCGI.py
+++ mabsCommandServerCGI.py
@@ -1,44 +1,52 @@
 #!/usr/bin/python2.2
-from CGIXMLRPCServer import SimpleXMLRPCServer233
-import socket,popen2
+import socket,popen2,time
 
 #
 # $Log$
+# Revision 1.1  2004/02/20 09:24:34  kpalin
+# Version that sort of takes care of the file serving by it self.
+# Doesn't really work.
+#
 #
 
+try:
+    from SimpleXMLRPCServer import CGIXMLRPCRequestHandler
+except ImportError:
+    from SimpleXMLRPCServer233 import CGIXMLRPCRequestHandler
 
-
+    
 hostname=socket.gethostname()
 portno=8001
 
-FileList=eval(open("serverFiles.py").read())
 
-FileList.reverse()
+errstrm=open("mabsCom.err","a")
+try:
+    import socket,popen2,os
 
-def fileExists(fname):
-    try:
-        os.stat(fname)
-        return 1
-    except OSError:
-        return 0
 
-import sys,os
-def nextFasta():
-    global FileList
-    try:
-        while 1:
-            filen=FileList.pop()
-            if (not fileExists("/home/kpalin/fs/tyot/mabs/synteny/output/good/%s.align.gff.gz"%filen)) and \
-               (not fileExists("/home/kpalin/fs/tyot/mabs/synteny/output/%s.align.gff.gz"%filen)):
-                break
-            else:
-                print >> sys.stderr, "Skipping",filen
-        print >> sys.stderr, filen
-        return filen
-    except IndexError:
-        return "xAllDone"
-    
+    from xmlrpclib import ServerProxy
 
-handler = CGIXMLRPCRequestHandler()
-handler.register_function(nextFasta)
-handler.handle_request()
+
+    servName=socket.gethostname()
+    servPort=int(open("mabsCommandServer.portno").read())
+
+
+    server=ServerProxy("http://%s:%d/"%(servName,servPort))
+
+    def nextFasta(clientID):
+        try:
+            filen=server.nextFasta(clientID)
+            print >> errstrm, "Serving",filen,"to",os.environ["REMOTE_HOST"],"(",clientID,") at", time.asctime()
+            errstrm.flush()
+            return  filen
+        
+        except Exception,e:
+            print >> errstrm,e
+            return "xTryAgain"
+
+
+    handler = CGIXMLRPCRequestHandler()
+    handler.register_function(nextFasta)
+    handler.handle_request()
+except Exception,e:
+    print >> errstrm,e
