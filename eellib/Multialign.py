8d56ab79166999de59e1a5fc9bb612fed0d88fd5
kpalin <>
2004-03-01 10:19:18 +0000

    Starting to implement an other aproach by Prohaska et.al.

--- eellib/Multialign.py
+++ eellib/Multialign.py
@@ -2,6 +2,9 @@
 
 #
 # $Log$
+# Revision 1.2  2004/02/23 12:23:52  kpalin
+# Updates for per gene orthologous runs. Maybe litle multiple alignment.
+#
 # Revision 1.1  2004/02/19 13:17:51  kpalin
 # Documentation for the main parts of multiple alignment
 # done. Now we only need the code.
@@ -12,6 +15,13 @@
 import xreadlines,re
 from StringIO import StringIO
 
+class NotComparable(Exception):
+    def __init__(self,f,s):
+        self.first=f
+        self.second=s
+
+    def __str__(self):
+        return "Can't compare %s <=> %s"%(str(self.first),str(self.second))
 
 class PairModule:
     """A pairwise cis module from a GFF file.
@@ -46,14 +56,22 @@ class PairModule:
     def __cmp__(self,other):
         """Comparison between two PairModule:s
 
-        The PairModules are ordered in lexicographic order with tuple
-        spos1,epos1,spos2,epos2  where the sequences are in alphabetic
-        order."""
-        r=cmp(self.DNApos[self.sNames[0]],other.DNApos[other.sNames[0]])
+        The PairModules are partially ordered along their alphabetically
+        first common sequence."""
+        haveCommon=1
+        for common in self.sNames:
+            if common in other.sNames:
+                haveCommon=1
+                break
+        try:
+            assert(haveCommon==1)
+        except AssertionError:
+            raise NotComparable(self,other)
+            
+        r=cmp(self.DNApos[common],other.DNApos[common])
         if r==0:
-            r=cmp(self.DNApos[self.sNames[1]],other.DNApos[other.sNames[1]])
-        if self.id==other.id:
-            assert(self.sNames==other.sNames)
+            r=cmp(self.sNames,other.sNames)
+
         return r
 
     def intersects(self,other):
@@ -61,7 +79,19 @@ class PairModule:
 
         Return true if the modules share a common TF binding site,
         false otherwise."""
-        pass
+        for common in self.sNames:
+            if other.DNApos.has_key(common):
+                break
+        try:
+            assert(self.DNApos[common][0]<other.DNApos[common][0]<self.DNApos[common][1])
+        except AssertionError,KeyError:
+            return 0
+        for (pos,sites) in zip(self.sitePos,self.sites):
+            for p,s in zip(other.sitePos,other.sites):
+                if p==pos and sites.match(s):
+                    return 1
+        return 0
+                
 
     def appendSite(self,sPos,site):
         """Append a new pair of sites belonging to this cis module.
@@ -71,7 +101,8 @@ class PairModule:
         identification."""
         for seq,pos in sPos.items():
             if seq==self.sNames[0]:
-                self.sitePos[seq].append((pos,site))
+                self.sitePos[seq].append(pos)
+                self.sites.append(site)
             else:
                 self.sitePos[seq].append(pos)                
         pass
@@ -80,7 +111,22 @@ class PairModule:
         for seq in self.sNames:
             self.sitePos[seq].sort()
 
+
     def __str__(self):
+        self._finalize()
+        
+        strbuf=StringIO()
+        
+        cmFormat='%s\tmalign\tCisModule\t%d\t%d\t%g\t.\t.\tTarget "%s";\tStart %d;\tEnd %d;\tStrand .;\tFrame .;\tCM %s;\n'
+
+        seq1,seq2=self.sNames
+        strbuf.write(cmFormat%(seq1,self.DNApos[seq1][0],self.DNApos[seq1][1],self.score,seq2,self.DNApos[seq2][0],self.DNApos[seq2][1],self.id))
+        strbuf.write(cmFormat%(seq2,self.DNApos[seq2][0],self.DNApos[seq2][1],self.score,seq1,self.DNApos[seq1][0],self.DNApos[seq1][1],self.id))
+
+        return strbuf.getvalue()
+
+    def toGFF(self):
+        """Returns GFF representation of the pairModule."""
         self._finalize()
         
         strbuf=StringIO()
@@ -91,7 +137,8 @@ class PairModule:
 
         siteFormat='%s\tmalign\t%s\t%d\t%d\t%g\t.\t.\tCM %s;\n'
         for i in range(len(self.sitePos[seq1])):
-            pos1,site=self.sitePos[seq1][i]
+            pos1=self.sitePos[seq1][i]
+            site=self.sites[i]
             strbuf.write(siteFormat%(seq1,site.name,pos1,pos1+site.width,site.score,self.id))
             pos2=self.sitePos[seq2][i]
             strbuf.write(siteFormat%(seq2,site.name,pos2,pos2+site.width,site.score,self.id))
@@ -108,7 +155,14 @@ class MultiModule:
         """Minimal constructor.
 
         Most work is done while adding the pairwise alignments."""
+        self._TFseqs={}
+        self.sites=[]
+        self._pairs={}
         pass
+
+
+    def __str__(self):
+        return str(self._pairs.items())
 
     def addPair(self,pair):
         """Adds a PairModule to this multiple alignment.
@@ -116,7 +170,20 @@ class MultiModule:
         The multiple alignment is formed by aligning all pairs
         of the sequences in the alignment with each other. These
         pairwise alignments are done before the multiple alignment."""
-        pass
+        r=0
+        if len(self._pairs)==0:
+            self._TFseqs=pair.sitePos.copy()
+            self._sites=pair.sites[:]
+            r=1
+            for name in pair.sNames:
+                if not self._pairs.has_key(name):
+                    self._pairs[name]=[]
+                self._pairs[name].append(pair)
+        elif self._TFseqs.has_key(pair.sNames[0]) and self._TFseqs.has_key(pair.sNames[1]):
+            r=self.strengthenAlignment(pair)
+        elif self._TFseqs.has_key(pair.sNames[0]) or self._TFseqs.has_key(pair.sNames[1]):
+            r=self.enlargeAlignment(pair)
+        return r
 
     def enlargeAlignment(self,pair):
         """Adds a new sequence to the multiple alignment.
@@ -124,7 +191,39 @@ class MultiModule:
         Adds a new PairModule to the MultiModule such that
         the PairModule contains a sequence not yet in MultiModule.
         This gives a new sequence to the multiple alignment."""
-        pass
+
+        # Do we have a common sequence?
+        haveKey=0
+        for common in pair.sNames:
+            if self._pairs.has_key(common):
+                haveKey=1
+                break
+        if haveKey==0:
+            return None
+
+        # Do we have an overlapping pairwise alignment?
+        intersects=0
+        for p in self._pairs[common]:
+            intersects=p.intersects(pair)
+            if intersects:
+                break
+        if intersects==0:
+            return None
+
+        for name in pair.sNames:
+            if not self._pairs.has_key(name):
+                self._pairs[name]=[]
+            self._pairs[name].append(pair)
+
+        i=0
+        TFseqs={}
+        for k in self._TFseqs.keys():
+            TFseqs[k]=0
+        while i<len(self._sites):
+            for k in TFseqs.keys():
+                
+            
+        return self
     
     def strengthenAlignment(self,pair):
         """Adds a new alignment between pairs that are already
@@ -132,9 +231,27 @@ class MultiModule:
 
         With this method we should add alignments between all
         sequences in the alignment."""
-        pass
-    
+        seq1,seq2=pair.sNames
+        for p in self._pairs[seq1]:
+            intersects=p.intersects(pair)
+            if intersects:
+                break
+        if intersects==0:
+            return None
+        intersects=0
+        for q in self._pairs[seq2]:
+            intersects=q.intersects(pair)
+            if intersects:
+                break
+        if intersects==0:
+            return None
 
+        for name in pair.sNames:
+            if not self._pairs.has_key(name):
+                self._pairs[name]=[]
+            self._pairs[name].append(pair)
+        return self
+    
 
 class Site:
     """Represents a binding site on the alignment"""
@@ -144,6 +261,9 @@ class Site:
         self.strand=strand
         self.width=int(width)
 
+    def match(self,other):
+        """Return true if two sites are for the same Transcription Factor"""
+        return self.name==other.name and self.strand==other.strand and self.width==other.width
 
 
 class MultipleAlignment:
@@ -157,6 +277,7 @@ class MultipleAlignment:
         "Minimal constructor"
         self._modules=[]
         self._attrPat=re.compile(r"([A-Za-z][A-Za-z0-9_]+) (.*)")
+        self._multiAlign=[]
         pass
 
 
@@ -217,8 +338,7 @@ class MultipleAlignment:
         The given GFF file should follow the format produced
         by mabs align command."""
         self._parseGFFfile(fname)
-        pass
-
+                
     def giveSequences(self):
         """Return information about sequences in the added GFF files.
 
@@ -244,6 +364,20 @@ class MultipleAlignment:
 
         The input and output is done through instance of this class with
         other methods."""
+        
+        # Add the pair modules in decreasing order of alignment score
+        self._modules.sort(lambda x,y:cmp(x.score,y.score))
+
+        while len(self._modules)>0:
+            r=0
+            p=self._modules.pop()
+            for mmod in self._multiAlign:
+                if mmod.addPair(p):
+                    r=1
+            if r==0:
+                nMulti=MultiModule()
+                nMulti.addPair(p)
+                self._multiAlign.append(nMulti)
 
     def __str__(self):
         strbuf=StringIO()
@@ -255,6 +389,11 @@ class MultipleAlignment:
 
 if __name__=="__main__":
     a=MultipleAlignment()
-    a.addGFFfile("/home/kpalin/tyot/comparative/mabs/fly.abs9.align.gff")
+    #a.addGFFfile("/home/kpalin/tyot/comparative/mabs/fly.abs9.align.gff")
+    a.addGFFfile("/home/kpalin/tyot/comparative/mabs/hmmyf5.align.opt.gff")
+    a.addGFFfile("/home/kpalin/tyot/comparative/mabs/hrmyf5.align.opt.gff")
+    #a.addGFFfile("/home/kpalin/tyot/comparative/mabs/mrmyf5.align.opt.gff")
     a._modules.sort()
     print str(a)
+    print a.giveSequences()
+    a.multiAlign()
