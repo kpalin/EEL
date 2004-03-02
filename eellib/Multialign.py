7bd7008ca15c1f96f06b7efd2be198a9d282cd8e
kpalin <>
2004-03-02 10:45:56 +0000

    Working version. It doesn't give proper output yet and it's not integrated to the mabs interface but it mostly works.

--- eellib/Multialign.py
+++ eellib/Multialign.py
@@ -2,6 +2,9 @@
 
 #
 # $Log$
+# Revision 1.3  2004/03/01 10:19:18  kpalin
+# Starting to implement an other aproach by Prohaska et.al.
+#
 # Revision 1.2  2004/02/23 12:23:52  kpalin
 # Updates for per gene orthologous runs. Maybe litle multiple alignment.
 #
@@ -79,17 +82,40 @@ class PairModule:
 
         Return true if the modules share a common TF binding site,
         false otherwise."""
+        #print "Intersecting",self.sNames,other.sNames
         for common in self.sNames:
             if other.DNApos.has_key(common):
                 break
         try:
-            assert(self.DNApos[common][0]<other.DNApos[common][0]<self.DNApos[common][1])
-        except AssertionError,KeyError:
+            # No overlap if self ends before beginning of other
+            # or starts after the end of the other
+            if self.DNApos[common][1]<other.DNApos[common][0] or \
+               self.DNApos[common][0]>other.DNApos[common][1]:
+                #print "No overlap on",common
+                #print self.DNApos[common][1],"<",other.DNApos[common[0]],"or",
+                #print self.DNApos[common][0],">",other.DNApos[common[1]]
+                return 0
+        except KeyError:
+            #import traceback
+            #traceback.print_exc()
+            #print "No common sequence in",self.sNames,other.sNames
             return 0
-        for (pos,sites) in zip(self.sitePos,self.sites):
-            for p,s in zip(other.sitePos,other.sites):
-                if p==pos and sites.match(s):
+
+        i,j=0,0
+        #print self.sitePos
+        while i<len(self.sites) and j<len(other.sites):
+            if self.sitePos[common][i]<other.sitePos[common][j]:
+                i+=1
+            elif self.sitePos[common][i]>other.sitePos[common][j]:
+                j+=1
+            else:
+                #print self.sites[i],other.sites[j],self.sites[i].match(other.sites[j])
+                if self.sites[i].match(other.sites[j]):
                     return 1
+                else:
+                    i+=1
+                    j+=1
+        #print "No common site on",common
         return 0
                 
 
@@ -157,12 +183,16 @@ class MultiModule:
         Most work is done while adding the pairwise alignments."""
         self._TFseqs={}
         self.sites=[]
-        self._pairs={}
+        self._pairsBySeq={}
+        self._pairs=[]
+
+        # Adjacency matrix for overlap graph
+        self._overlaps={}
         pass
 
 
     def __str__(self):
-        return str(self._pairs.items())
+        return str(self._pairsBySeq.items())
 
     def addPair(self,pair):
         """Adds a PairModule to this multiple alignment.
@@ -170,89 +200,218 @@ class MultiModule:
         The multiple alignment is formed by aligning all pairs
         of the sequences in the alignment with each other. These
         pairwise alignments are done before the multiple alignment."""
-        r=0
-        if len(self._pairs)==0:
-            self._TFseqs=pair.sitePos.copy()
-            self._sites=pair.sites[:]
-            r=1
-            for name in pair.sNames:
-                if not self._pairs.has_key(name):
-                    self._pairs[name]=[]
-                self._pairs[name].append(pair)
-        elif self._TFseqs.has_key(pair.sNames[0]) and self._TFseqs.has_key(pair.sNames[1]):
-            r=self.strengthenAlignment(pair)
-        elif self._TFseqs.has_key(pair.sNames[0]) or self._TFseqs.has_key(pair.sNames[1]):
-            r=self.enlargeAlignment(pair)
-        return r
 
-    def enlargeAlignment(self,pair):
-        """Adds a new sequence to the multiple alignment.
 
-        Adds a new PairModule to the MultiModule such that
-        the PairModule contains a sequence not yet in MultiModule.
-        This gives a new sequence to the multiple alignment."""
-
-        # Do we have a common sequence?
-        haveKey=0
-        for common in pair.sNames:
-            if self._pairs.has_key(common):
-                haveKey=1
-                break
-        if haveKey==0:
-            return None
-
-        # Do we have an overlapping pairwise alignment?
-        intersects=0
-        for p in self._pairs[common]:
-            intersects=p.intersects(pair)
-            if intersects:
-                break
-        if intersects==0:
-            return None
+        n=len(self._pairs)
+        # The first pairwise alignment.
+        if n>0:
+            # Do we have a common sequence?
+            haveKey=0
+            intersects=0
+            for common in pair.sNames:
+                if self._pairsBySeq.has_key(common):
+                    # Do we have an overlapping pairwise alignment?
+                    ## Bin search??
+                    for opair,code in self._pairsBySeq[common]:
+                        isects=opair.intersects(pair)
+                        if isects:
+                            intersects=1
+                            self._overlaps[(code,n)]=n
+                            self._overlaps[(n,code)]=n
+            if not intersects:
+                return 0
 
         for name in pair.sNames:
-            if not self._pairs.has_key(name):
-                self._pairs[name]=[]
-            self._pairs[name].append(pair)
+            if not self._pairsBySeq.has_key(name):
+                self._pairsBySeq[name]=[]
+            self._pairsBySeq[name].append((pair,n))
+        self._pairs.append(pair)
+        
+        return 1                
+        
 
-        i=0
-        TFseqs={}
-        for k in self._TFseqs.keys():
-            TFseqs[k]=0
-        while i<len(self._sites):
-            for k in TFseqs.keys():
-                
+
+    def TransitiveClosure(self):
+        """Computes the transitive closure of the overlap graph.
+
+        Uses the Floyd-Warshall algorithm for all-pairs-shortest-path
+        problem with time complexity O(n^3). In theory, that can be
+        improved to about O(n^2.3). The implementation is unnecessary
+        strickt about having directed edges to both directions. This
+        could, and should, be improved."""
+
+
+        self._TC=self._overlaps.copy()  # Transitive closure
+        n=len(self._pairs)
+        if n<=2:
+            return
+
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    
+                    if self._TC.has_key((i,k)) and self._TC.has_key((k,j)):
+                        self._TC[(i,j)]=1
+
+    def InconsistencyGraph(self):
+        """Computes the inconsistency graph for this multi module.
+
+        This part too unnecessarily assumes directed graphs."""
+
+        self._inconsist={}
+        if len(self._pairs)<=2:
+            return
+        for i,j in self._TC.keys():
+            isect=0
+            for n in self._pairs[i].sNames:
+                if self._pairs[j].DNApos.has_key(n):
+                    isect+=1
+            if isect==2 or (isect==1 and not self._overlaps.has_key((i,j))):
+                self._inconsist[(i,j)]=1
+
+
+    def connected(self,i,j):
+        """Checks whether i and j are connected in the consistency graph."""
+        return i==j or not self._inconsist.has_key((i,j))
+
+    def cliquesToDot(self):
+        strbuf=StringIO()
+        strbuf.write("graph G {\n")
+        N=len(self._pairs)
+        if hasattr(self,"_cliques"):
+            for j in range(len(self._cliques)):
+                clq=self._cliques[j]
+                strbuf.write('subgraph cluster%d {\nlabel="Clique %s";\n%s;\n\n}'%(j,j,";\n".join(["n%dC%d"%(x,j) for x in clq])))
+                for i in range(N):
+                    try:
+                        strbuf.write('n%dC%d [label="%s-%s CM%s"];\n'%(i,j,self._pairs[i].sNames[0][:3],self._pairs[i].sNames[1][:3],str(self._pairs[i].id[-3:])))
+                    except AttributeError:
+                        pass
+                    for k in range(i,N):
+                        if i!=k and self._overlaps.has_key((i,k)):
+                            strbuf.write("n%dC%d -- n%dC%d;\n"%(i,j,k,j))
+                            if not self.connected(i,k):
+                                strbuf.write("n%dC%d -- n%dC%d [color=red];\n"%(i,j,k,j))
+                #strbuf.write("}\n")
+
+        strbuf.write("}\n")
+        return strbuf.getvalue()
+
+    def inconsistToDot(self):
+        """Returns the inconsistency graph in dot format.
+
+        Dot is an graph layout program which is also known as graphviz.
+        It is available for free from AT&T."""
+        strbuf=StringIO()
+        strbuf.write("graph G {\n")
+        N=len(self._pairs)
+        
+        for i in range(N):
+            try:
+                strbuf.write('n%d [label="%s-%s CM%s"];\n'%(i,self._pairs[i].sNames[0][:3],self._pairs[i].sNames[1][:3],str(self._pairs[i].id[-3:])))
+            except AttributeError:
+                pass
+            for j in range(i,N):
+                if i!=j and self.connected(i,j):
+                    strbuf.write("n%d -- n%d;\n"%(i,j))
+
+        strbuf.write("}\n")
+        return strbuf.getvalue()
+                                 
+
+    def allMaxCliques(self):
+        """Compute all maximal cliques in the complement of the inconsistency graph.
+
+        This uses the Bron, Kerbosch (CACM 1973: 16(9) p.575-7) algorithm
+        to solve the NP complete problem (in worst case exponential time)"""
+
+        N=len(self._pairs)
+
+        if N<=2:
+            self._cliques=[range(N)]
+            return
+
+        def extend2(old,ne,ce):
+            """Extend procedure from Bron and Kerbosch"""
+            #print old,ne,ce
+            new=range(ce)
+            minnod,nod=ce,0
+
+            # Determine each counter value and look for minimum
+            for i in range(ce):
+                if minnod==0:
+                    break
+                p,count=old[i],0
+
+                # Count disconnections
+                for j in range(ne,ce):
+                    if count >= minnod:
+                        break
+                    if not self.connected(p,old[j]):
+                        count+=1
+                        # Save position for potential candidate
+                        pos=j
+
+                # Test new minimum
+                if count<minnod:
+                    fixp=p
+                    minnod=count
+                    if i<ne:
+                        s=pos
+                    else:
+                        s=i
+                        nod=1
+                    # Note: If fixed point initially chosen from candidates
+                    # then number of disconnections will be preincreased by
+                    # one
+
+            # Backtrack cycle
+            for nod in range(minnod+nod,0,-1):
+                # Interchange
+                old[s],old[ne]=old[ne],old[s]
+                sel=old[ne]
+                # Fill new set NOT
+                newne=0
+                for i in range(ne):  ## KJP: Make this with filter()
+                    if self.connected(sel,old[i]):
+                        new[newne]=old[i]
+                        newne+=1
+                        #print "n%d n%d connected, newne=%d"%(sel,old[i],newne)
+                    #else:
+                        #print "n%d n%d not connected ne=%d"%(sel,old[i],ne)
+                # Fill new set CAND
+                newce=newne
+                for i in range(ne+1,ce):
+                    if self.connected(sel,old[i]):
+                        new[newce]=old[i]
+                        newce=newce+1
+                # Add to COMPSUB
+                compsub[self.c]=sel;
+                self.c+=1
+
+                if newce==0:
+                    self._cliques.append(compsub[:self.c])
+                elif newne<newce:
+                    extend2(new,newne,newce)
+                # Remove from COMPSUB
+                self.c-=1
+                # Add to NOT
+                ne+=1
+                if nod>1:
+                    # select a candidate disconnected to the fixed point
+                    s=ne
+                    # look for candidate
+                    while self.connected(fixp,old[s]):
+                        s+=1
             
-        return self
-    
-    def strengthenAlignment(self,pair):
-        """Adds a new alignment between pairs that are already
-        in the multiple alignment.
+                        
+        compsub=[-1]*N
 
-        With this method we should add alignments between all
-        sequences in the alignment."""
-        seq1,seq2=pair.sNames
-        for p in self._pairs[seq1]:
-            intersects=p.intersects(pair)
-            if intersects:
-                break
-        if intersects==0:
-            return None
-        intersects=0
-        for q in self._pairs[seq2]:
-            intersects=q.intersects(pair)
-            if intersects:
-                break
-        if intersects==0:
-            return None
+        self._cliques=[]
+        self.c=0
+        extend2(range(N),0,N)
 
-        for name in pair.sNames:
-            if not self._pairs.has_key(name):
-                self._pairs[name]=[]
-            self._pairs[name].append(pair)
-        return self
-    
-
+        
 class Site:
     """Represents a binding site on the alignment"""
     def __init__(self,name,score,strand,width):
@@ -265,7 +424,11 @@ class Site:
         """Return true if two sites are for the same Transcription Factor"""
         return self.name==other.name and self.strand==other.strand and self.width==other.width
 
+    def __str__(self):
+        return "%s %s %d %g"%(self.name,self.strand,self.width,self.score)
 
+    def __repr__(self):
+        return "Site(%s,%g,%s,%d)"%(self.name,self.score,self.strand,self.width)
 class MultipleAlignment:
     """Main class for multiple alignment.
 
@@ -306,13 +469,12 @@ class MultipleAlignment:
         currMod=None
         currModId=""
         for line in xreadlines.xreadlines(fhandle):
-            if len(line)<1 or line[0]=='#':  ## Skip empty and comment lines.
+            line=line.strip()
+            if len(line)==0 or line[0]=='#':  ## Skip empty and comment lines.
                 continue
             ## Parse a GFF line
-            line=line.strip()
             seq,src,feat,start,stop,score,strand,frame,attribs=line.split('#')[0].split("\t",8)
             attribs=self._parseAttribs(attribs)
-
             lineModId=fname+attribs["CM"]
             # Start of a new cis module
             if feat=='CisModule':
@@ -365,9 +527,6 @@ class MultipleAlignment:
         The input and output is done through instance of this class with
         other methods."""
         
-        # Add the pair modules in decreasing order of alignment score
-        self._modules.sort(lambda x,y:cmp(x.score,y.score))
-
         while len(self._modules)>0:
             r=0
             p=self._modules.pop()
@@ -378,7 +537,13 @@ class MultipleAlignment:
                 nMulti=MultiModule()
                 nMulti.addPair(p)
                 self._multiAlign.append(nMulti)
-
+        # Multiple alignment by Prohaska et.al.
+        for mmod in self._multiAlign:            
+            mmod.TransitiveClosure()
+            mmod.InconsistencyGraph()
+            mmod.allMaxCliques()
+            
+            
     def __str__(self):
         strbuf=StringIO()
         for mod in self._modules:
@@ -389,11 +554,17 @@ class MultipleAlignment:
 
 if __name__=="__main__":
     a=MultipleAlignment()
-    #a.addGFFfile("/home/kpalin/tyot/comparative/mabs/fly.abs9.align.gff")
+    #a.addGFFfile("/home/kpalin/tyot/comparative/mabs/goodPairAligns.gff")
     a.addGFFfile("/home/kpalin/tyot/comparative/mabs/hmmyf5.align.opt.gff")
     a.addGFFfile("/home/kpalin/tyot/comparative/mabs/hrmyf5.align.opt.gff")
-    #a.addGFFfile("/home/kpalin/tyot/comparative/mabs/mrmyf5.align.opt.gff")
-    a._modules.sort()
-    print str(a)
-    print a.giveSequences()
+    a.addGFFfile("/home/kpalin/tyot/comparative/mabs/mrmyf5.align.opt.gff")
+    #a._modules.sort()
     a.multiAlign()
+    p=[x._pairs[0] for x in a._multiAlign]
+    import pdb
+    for b in range(len(a._multiAlign)):
+        if len(a._multiAlign[b]._pairs)<2:
+            continue
+        open("tmp%d.dot"%(b),"w").write(a._multiAlign[b].cliquesToDot())
+
+    #pdb.run("a.multiAlign()")
