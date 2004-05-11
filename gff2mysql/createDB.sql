e73bf8b25b91ceb52ce96ccd080161edb529de6c
kpalin <>
2004-05-11 06:11:25 +0000

    The type of schema currently used with fast data loading thingy.

--- gff2mysql/createDB.sql
+++ gff2mysql/createDB.sql
@@ -1,20 +1,99 @@
-/*
-$Log$
-*/
 DROP TABLE IF EXISTS sequence;
 DROP TABLE IF EXISTS region;
 DROP TABLE IF EXISTS cismodule;
 DROP TABLE IF EXISTS sites;
 DROP TABLE IF EXISTS tfactor;
 DROP TABLE IF EXISTS pairs;
+DROP TABLE IF EXISTS alnCols;
+DROP TABLE IF EXISTS gfffile;
 
+
+
+
+
+drop table if exists sequence;
+drop table if exists region;
+drop table if exists cismodule;
+drop table if exists tfactor;
+drop table if exists sliceCisLink;
+drop table if exists slice;
+DROP TABLE IF EXISTS alnCols ;
+
+drop table if exists sites;
+drop table if exists gene_description;
+
+create table gene_description(
+	enscode varchar(40),
+	modified date,
+	description text,
+        KEY (enscode),
+	FOREIGN KEY (enscode) REFERENCES slice(enscode)
+);
 
 CREATE TABLE sequence (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	species VARCHAR(255),
-	DBname VARCHAR(255),
-	name VARCHAR(255)
+	species VARCHAR(125),
+	DBname VARCHAR(125) NOT NULL,
+	name VARCHAR(125) NOT NULL,
+	UNIQUE (name,species,DBname)
 );
+
+
+-- mysqlimport -p  bbu_palin -v --local gff.txt
+
+CREATE TABLE slice (
+  id int(11) AUTO_INCREMENT PRIMARY KEY,
+  beginPos int(11) NOT NULL default '0',
+  endPos int(11) NOT NULL default '0',
+  fileName varchar(200) NOT NULL default '',
+  enscode varchar(40),
+	geneName varchar(20),
+  seqName varchar(200),
+  revComplement int(11) NOT NULL default 0,
+	seqID INT,
+	KEY (seqID),
+  KEY `geneIndex` (`enscode`),
+  KEY (fileName,seqName)
+);
+
+
+create table cismodule (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	score FLOAT,
+	tmpID varchar(255),
+	KEY (tmpID)
+);
+
+	
+CREATE TABLE sliceCisLink (
+	sliceID int not null,
+	cisID int not null,
+	KEY (sliceID,cisID),
+	PRIMARY KEY (cisID,sliceID)
+);
+
+
+CREATE TABLE alnCols (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	tfID INT,
+	scoredelta FLOAT,
+	cisID INT NOT NULL,
+	cisColId INT NOT NULL,
+	KEY (cisID,cisColId),
+	KEY (tfID)
+);
+
+CREATE TABLE sites (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	weight FLOAT,
+	pos INT,
+	strand CHAR(1),
+	regID INT NOT NULL,
+	colID INT NOT NULL,
+	KEY (regID),
+	KEY (colID)
+);
+
 
 
 CREATE TABLE region (
@@ -23,42 +102,18 @@ CREATE TABLE region (
 	beginPos INT,
 	endPos INT,
 	cisID INT,
-	FOREIGN KEY (seqID) REFERENCES sequence(id)
-	FOREIGN KEY (cisID) REFERENCES cismodule(id)
-);
-
-CREATE TABLE cismodule (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	score FLOAT,
-);
-
-
-CREATE TABLE column (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	tfID INT,
-	strand TINYINT, /* 0 watson, 1 crick */
-	scoredelta FLOAT,
-	FOREIGN KEY (tfID) REFERENCES tfactor(id)
-);
-	
-
-
-
-
-CREATE TABLE sites (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	weight FLOAT,
-	pos INT,
-	regID INT,
-	colID INT,
-	FOREIGN KEY (regID) REFERENCES region(id),
-	FOREIGN KEY (colID) REFERENCES column(id)
+	KEY (seqID),
+	KEY (cisID)
 );
 
 
 CREATE TABLE tfactor (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	width INT,
-	name VARCHAR(255)
+	name VARCHAR(255) UNIQUE,
+	KEY (name)
 );
+
+
+
 
