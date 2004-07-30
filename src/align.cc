5ff6a296d4bd1b1ec4a803f12f5592428e013321
kpalin <>
2004-07-30 12:07:50 +0000

    Output changed to use alnColumn objects.

--- src/align.cc
+++ src/align.cc
@@ -30,10 +30,17 @@
 #endif
 
 
+#include "alignedCols.h"
+
 
 /*
  *
  *  $Log$
+ *  Revision 1.10  2004/02/26 11:27:36  kpalin
+ *  Changed the output from nextBest() function to be new SitePair objects
+ *  instead a large tuple of numbers. It's easier to change in the future
+ *  and easier to use on python side.
+ *
  *  Revision 1.9  2004/02/13 09:40:25  kpalin
  *  Corrected bugs
  *
@@ -448,6 +455,7 @@ typedef struct {
 
     /* Type-specific fields go here. */
   PyStringObject *x_name,*y_name;
+  PyObject *names;
   PyStringObject *output;
   PyObject *bestAlignments;
 
@@ -467,136 +475,6 @@ typedef struct {
   struct __CPSTUF *CP;
 
 } align_AlignmentObject;
-
-
-//////////////////////////////////////////////////////////////////////
-// Aligned site object
-
-typedef struct {
-  PyObject_HEAD
-
-    /* Type-specific fields go here. */
-
-  char *motifName;
-  int seqX;
-  int seqY;
-  int beginX;
-  int endX;
-  int beginY;
-  int endY;
-  char strand;
-  double score;
-  double siteScoreX;
-  double siteScoreY;
-} align_siteObject;
-
-static PyMemberDef site_members[] = {
-    {"motif",T_STRING, offsetof(align_siteObject, motifName), 0,
-     "Name of the motif."},
-
-    {"seqX",T_INT, offsetof(align_siteObject, seqX), 0,
-     "Position on site sequence x."},
-    {"seqY",T_INT, offsetof(align_siteObject, seqY), 0,
-     "Position on site sequence y."},
-
-    {"beginX",T_INT, offsetof(align_siteObject, beginX), 0,
-     "Begin position on DNA sequence x."},
-
-    {"beginY",T_INT, offsetof(align_siteObject, beginY), 0,
-     "Begin position on DNA sequence y."},
-
-    {"endX",T_INT, offsetof(align_siteObject, endX), 0,
-     "End position on DNA sequence x."},
-
-    {"endY",T_INT, offsetof(align_siteObject, endY), 0,
-     "End position on DNA sequence y."},
-    {"strand",T_CHAR, offsetof(align_siteObject, strand), 0,
-
-     "Strand of the motif."},
-    {"score",T_DOUBLE, offsetof(align_siteObject, score), 0,
-     "Alignment score this far."},
-    {"siteScoreX",T_DOUBLE, offsetof(align_siteObject, siteScoreX), 0,
-     "Score of the site on sequence X."},
-    {"siteScoreY",T_DOUBLE, offsetof(align_siteObject, siteScoreY), 0,
-     "Score of the site on sequence Y."},
-    {NULL}  /* Sentinel */
-};
-
-
-static void site_dealloc(align_siteObject* self)
-{
-  delete [] self->motifName;
-
-  self->ob_type->tp_free((PyObject*)self);
-}
-
-static PyTypeObject align_siteType = {
-    PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "align.SitePair",             /*tp_name*/
-    sizeof(align_siteObject), /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    (destructor)site_dealloc,                         /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
-    "Site pair object",           /* tp_doc */
-    0,		               /* tp_traverse */
-    0,		               /* tp_clear */
-    0,		               /* tp_richcompare */
-    0,		               /* tp_weaklistoffset */
-    0,		               /* tp_iter */
-    0,		               /* tp_iternext */
-    0,             /* tp_methods */
-    site_members,             /* tp_members */
-};
-
-static PyObject *
-site_new(const char *motifName,
-	 int seqX,
-	 int seqY,
-	 int beginX,
-	 int endX,
-	 int beginY,
-	 int endY,
-	 char strand,
-	 double score,
-	 double siteScoreX,
-	 double siteScoreY)
-{
-  align_siteObject *ret;
-  ret=(align_siteObject*)align_siteType.tp_alloc(&align_siteType,0);
-
-  ret->motifName=new char[strlen(motifName)+1];
-  strcpy(ret->motifName,motifName);
-
-  ret->seqX=seqX;
-  ret->seqY=seqY;
-  ret->beginX=beginX;
-  ret->endX=endX;
-  ret->beginY=beginY;
-  ret->endY=endY;
-  ret->strand=strand;
-  ret->score=score;
-  ret->siteScoreX=siteScoreX;
-  ret->siteScoreY=siteScoreY;
-
-  return (PyObject*)ret;
-}
-
-//////////////////////////////////////////////////////////////////////
 
 
 
@@ -645,6 +523,10 @@ alignment_init(align_AlignmentObject *self, PyObject *args, PyObject *kwds)
         self->y_name = (PyStringObject*)y_name;
     }
 
+
+    Py_INCREF(x_name);
+    Py_INCREF(y_name);
+    self->names=Py_BuildValue("(SS)",x_name,y_name);
 
     if (output) {
         CHECKING_DECREF(self->output);
@@ -763,6 +645,10 @@ static PyMemberDef alignment_members[] = {
      "List of best alignments"},
     {"secs_to_align",T_DOUBLE, offsetof(align_AlignmentObject, secs_to_align), 0,
      "CPU time for Alignment"},
+    
+    {"names",T_OBJECT_EX, offsetof(align_AlignmentObject, names), 0,
+     "Names of the sequences, x=0, y=1"},
+    
     {"x_name",T_OBJECT_EX, offsetof(align_AlignmentObject, x_name), 0,
      "Name of the X string"},
     {"y_name",T_OBJECT_EX, offsetof(align_AlignmentObject, y_name), 0,
@@ -1785,20 +1671,18 @@ initalign(void)
     if (PyType_Ready(&align_AlignmentType) < 0)
       return;
 
-    align_siteType.tp_new = PyType_GenericNew;
-    if (PyType_Ready(&align_siteType) < 0)
-      return;
-    
     m=Py_InitModule("align", alignMethods);
 
     if(m==NULL)
       return;
 
+
+    if(import_alnCols()<0)
+      return;
+
     Py_INCREF(&align_AlignmentType);
     PyModule_AddObject(m, "Alignment", (PyObject *)&align_AlignmentType);
 
-    Py_INCREF(&align_siteType);
-    PyModule_AddObject(m, "SitePair", (PyObject *)&align_siteType);
 }
 
 
@@ -1886,7 +1770,7 @@ alignment_nextBest(align_AlignmentObject *self)
 #endif
 
 
-      PyObject *ret_item=site_new(self->CP->ID_to_TF[self->CP->seq_x[pos_x].ID/2].c_str(), // motif name
+      PyObject *ret_item=PyAln_New(self->CP->ID_to_TF[self->CP->seq_x[pos_x].ID/2].c_str(), // motif name
 				  pos_x, // on site sequence X
 				  real_y, // on site sequence Y
 				  (int)self->CP->seq_x[pos_x].pos, 
